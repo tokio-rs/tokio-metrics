f118888a80e0cf6fca4d19d228d44246139f5787
Carl Lerche <me@carllerche.com>
2021-12-23 16:03:50 -0800

    initial pass at runtime metrics

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
+
+macro_rules! cfg_rt {
+    ($($item:item)*) => {
+        $(
+            #[cfg(all(tokio_unstable, feature = "rt"))]
+            #[cfg_attr(docsrs, doc(cfg(all(tokio_unstable, feature = "rt"))))]
+            $item
+        )*
+    };
+}
+
+cfg_rt! {
+    pub mod runtime;
+    pub use runtime::RuntimeMetrics;
+}
+
 pub mod task;
 pub use task::{InstrumentedTask, TaskMetrics};
---
+++ src/runtime.rs
@@ -0,0 +1,94 @@
+use tokio::runtime;
+
+use std::time::{Duration, Instant};
+
+pub struct RuntimeMetrics {
+    runtime: runtime::Handle,
+}
+
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy)]
+pub struct Sample {
+    pub num_parks: u64,
+    pub num_steals: u64,
+    pub num_polls: u64,
+    pub total_time: Duration,
+    pub total_time_busy: Duration,
+}
+
+impl RuntimeMetrics {
+    pub fn new(runtime: &runtime::Handle) -> RuntimeMetrics {
+        RuntimeMetrics { runtime: runtime.clone() }
+    }
+
+    // Total number of times a runtime thread parked
+    pub fn num_parks(&self) -> u64 {
+        self.runtime.stats().workers().map(|w| w.park_count()).sum()
+    }
+
+    pub fn num_steals(&self) -> u64 {
+        self.runtime.stats().workers().map(|w| w.steal_count()).sum()
+    }
+
+    pub fn num_polls(&self) -> u64 {
+        self.runtime.stats().workers().map(|w| w.poll_count()).sum()
+    }
+
+    pub fn total_time_busy(&self) -> Duration {
+        self.runtime.stats().workers().map(|w| w.total_busy_duration()).sum()
+    }
+
+    fn probe(&self, started_at: Instant) -> Sample {
+        Sample {
+            num_parks: self.num_parks(),
+            num_steals: self.num_steals(),
+            num_polls: self.num_polls(),
+            total_time: started_at.elapsed(),
+            total_time_busy: self.total_time_busy(),
+        }        
+    }
+
+    pub fn sample(&self) -> impl Iterator<Item = Sample> {
+        struct Iter(RuntimeMetrics, Sample, Instant);
+
+        impl Iterator for Iter {
+            type Item = Sample;
+
+            fn next(&mut self) -> Option<Sample> {
+                let latest = self.0.probe(self.2);
+
+                let ret = Sample {
+                    num_parks: latest.num_parks - self.1.num_parks,
+                    num_steals: latest.num_steals - self.1.num_steals,
+                    num_polls: latest.num_polls - self.1.num_polls,
+                    total_time: latest.total_time - self.1.total_time,
+                    total_time_busy: latest.total_time_busy - self.1.total_time_busy,
+                };
+
+                self.1 = latest;
+                Some(ret)
+            }
+        }
+
+        let now = Instant::now();
+
+        Iter(RuntimeMetrics {
+            runtime: self.runtime.clone()
+        }, self.probe(now), now)
+    }
+}
+
+impl Sample {
+    pub fn mean_polls_per_park(&self) -> u64 {
+        if self.num_parks == 0 {
+            0
+        } else {
+            self.num_polls / self.num_parks
+        }
+    }
+
+    pub fn busy_ratio(&self) -> f64 {
+        self.total_time_busy.as_nanos() as f64 /
+            self.total_time.as_nanos() as f64
+    }
+}
