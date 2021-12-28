b765561eec1a17116a3c05c8717e09c40a694f3b
Carl Lerche <me@carllerche.com>
2021-12-28 15:07:58 -0800

    wip

--- src/runtime.rs
+++ src/runtime.rs
@@ -10,7 +10,9 @@ pub struct RuntimeMetrics {
 #[derive(Debug, Clone, Copy)]
 pub struct Sample {
     pub num_parks: u64,
+    pub num_noops: u64,
     pub num_steals: u64,
+    pub num_local_schedules: u64,
     pub num_polls: u64,
     pub total_time: Duration,
     pub total_time_busy: Duration,
@@ -26,8 +28,16 @@ impl RuntimeMetrics {
         self.runtime.stats().workers().map(|w| w.park_count()).sum()
     }
 
+    pub fn num_noops(&self) -> u64 {
+        self.runtime.stats().workers().map(|w| w.noop_count()).sum()
+    }
+
     pub fn num_steals(&self) -> u64 {
         self.runtime.stats().workers().map(|w| w.steal_count()).sum()
+    }
+
+    pub fn num_local_schedules(&self) -> u64 {
+        self.runtime.stats().workers().map(|w| w.local_schedule_count()).sum()
     }
 
     pub fn num_polls(&self) -> u64 {
@@ -41,7 +51,9 @@ impl RuntimeMetrics {
     fn probe(&self, started_at: Instant) -> Sample {
         Sample {
             num_parks: self.num_parks(),
+            num_noops: self.num_noops(),
             num_steals: self.num_steals(),
+            num_local_schedules: self.num_local_schedules(),
             num_polls: self.num_polls(),
             total_time: started_at.elapsed(),
             total_time_busy: self.total_time_busy(),
@@ -59,7 +71,9 @@ impl RuntimeMetrics {
 
                 let ret = Sample {
                     num_parks: latest.num_parks - self.1.num_parks,
+                    num_noops: latest.num_noops - self.1.num_noops,
                     num_steals: latest.num_steals - self.1.num_steals,
+                    num_local_schedules: latest.num_local_schedules - self.1.num_local_schedules,
                     num_polls: latest.num_polls - self.1.num_polls,
                     total_time: latest.total_time - self.1.total_time,
                     total_time_busy: latest.total_time_busy - self.1.total_time_busy,
@@ -79,11 +93,12 @@ impl RuntimeMetrics {
 }
 
 impl Sample {
-    pub fn mean_polls_per_park(&self) -> u64 {
-        if self.num_parks == 0 {
-            0
+    pub fn mean_polls_per_park(&self) -> f64 {
+        let num_parks = self.num_parks - self.num_noops;
+        if num_parks == 0 {
+            0.0
         } else {
-            self.num_polls / self.num_parks
+            self.num_polls as f64 / num_parks as f64
         }
     }
 
